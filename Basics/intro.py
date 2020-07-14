25ca62ba677ee33536965f30973cb3339654a766
Peter <Posoo617@gmail.com>
2020-07-14 08:58:51 +0300

    Slicing and scope(LEGB rules)

--- Basics/intro.py
+++ Basics/intro.py
@@ -1,3 +1,4 @@
+import antigravity
 print('Go Home')
 
 print('Trying python after a long while')
@@ -29,3 +30,22 @@ student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
 
 for key, value in student.items():
     print(key, value)
+
+
+# is keyword
+a = [1, 2, 3]
+b = a
+
+print(id(a))
+print(id(b))
+print(a is b)
+
+
+# while loop
+x = 0
+
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
