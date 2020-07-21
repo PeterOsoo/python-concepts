19f3b143afb51991022478f40d5aa727a3a2399c
Peter <Posoo617@gmail.com>
2020-07-21 08:34:03 +0300

    datetime module, os module & working with files

--- Basics/script.py
+++ Basics/script.py
@@ -3,6 +3,3 @@ import sys
 print(sys.version)
 print(sys.executable)
 
-
-name = input("Your name? ")
-print("Hello,", name)
---
+++ OS/os.py
@@ -0,0 +1,5 @@
+import os
+
+# print(os.environ.get(‘HOME’))
+
+print(dir(os.path))
