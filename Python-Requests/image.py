326f918386c36cf6af996764edf3add5a6498aee
Peter <Posoo617@gmail.com>
2020-09-27 12:30:33 +0300

    Download Images

---
+++ Python-Requests/image.py
@@ -0,0 +1,11 @@
+import requests
+
+
+r = requests.get('https://imgs.xkcd.com/comics/python.png')
+
+with open('comic.png', 'wb') as f:
+    f.write(r.content)
+
+
+# print(r.content)
+# print(r.headers)
