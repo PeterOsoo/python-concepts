4c530fc35374dc7532259eac8cc52299959f989d
Peter Osoo <Posoo617@gmail.com>
2020-10-25 13:55:26 +0300

    Tuples

---
+++ Basics/Lists, Tuples & Sets/tuples.py
@@ -0,0 +1,12 @@
+# Immutable
+tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+tuple_2 = tuple_1
+
+print(tuple_1)
+print(tuple_2)
+
+# TypeError: 'tuple' object does not support item assignment
+# tuple_1[0] = 'Art'
+
+print(tuple_1)
+print(tuple_2)
