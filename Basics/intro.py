8c1bd1bd96cdfac7a712f4aef77466ba78a58b0d
Peter Osoo <Posoo617@gmail.com>
2020-10-25 13:30:32 +0300

    Casting in numerical data

--- Basics/intro.py
+++ Basics/intro.py
@@ -1,51 +1,30 @@
+
 import antigravity
 print('Go Home')
 
 print('Trying python after a long while')
 
+
+# numeric data
 print(3 / 2)
 print(3 // 2)
+print(5 % 2)
 
 
 num_1 = 3
 num_2 = 4
 
 print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 >= num_2)
+print(num_1 < num_2)
+print(num_1 > num_2)
+print(num_1 <= num_2)
 
-# lists
-courses = ['History', 'Math', 'Physics', 'CompSci']
+num_3 = '100'
+num_4 = '300'
 
-# print(list_1)
+num_3 = int(num_3)
+num_4 = int(num_4)
 
-for item in courses:
-    print(item)
-
-
-# student = {'name': 'Jack', 'age': 25}
-
-# print(student.get('courses'))
-
-
-student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
-
-for key, value in student.items():
-    print(key, value)
-
-
-# is keyword
-a = [1, 2, 3]
-b = a
-
-print(id(a))
-print(id(b))
-print(a is b)
-
-
-# while loop
-x = 0
-
-while True:
-    if x == 5:
-        break
-    print(x)
-    x += 1
+print(num_3 + num_4)
