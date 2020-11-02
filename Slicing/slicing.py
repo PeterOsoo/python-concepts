e63d2aabb0c03f98fb38492c1e8d8a03709044c3
Peter Osoo <Posoo617@gmail.com>
2020-11-02 18:27:33 +0300

    Slicing Lists and Strings

--- Slicing/slicing.py
+++ Slicing/slicing.py
@@ -4,7 +4,12 @@ my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # list[start:end:step]
 
+# end index is  none inclusive
+
+print(my_list[5])
 # print my_list[::-1]
+print(my_list[2:-1:2])
+print(my_list[-1:2:-1])
 
 
 sample_url = 'http://ratengd.com'
