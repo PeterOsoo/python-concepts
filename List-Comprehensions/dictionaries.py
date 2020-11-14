c1601bd6f98c43bc00c3af67b9e154f32a58bac0
PeterOsoo <posoo617@gmail.com>
2020-11-14 20:06:36 +0300

    Zip function

---
+++ List-Comprehensions/dictionaries.py
@@ -0,0 +1,7 @@
+
+# Dictionary Comprehensions
+names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
+heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
+
+# zip function creates a list of tuples 
+print(zip(names, heros))
