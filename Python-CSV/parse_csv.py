358b43e4690bd99530ce29765ee9552c1544cb59
PeterOsoo <posoo617@gmail.com>
2020-11-22 11:14:36 +0300

    Write CSV Files

--- Python-CSV/parse_csv.py
+++ Python-CSV/parse_csv.py
@@ -3,7 +3,11 @@ import csv
 with open('names.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
-    next(csv_reader)
+    # next(csv_reader)
 
-    for line in csv_reader:
-        print(line[2])
+    # open new file for writing
+    with open('new_names.csv', 'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter='\t')
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
