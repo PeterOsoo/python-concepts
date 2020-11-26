5cc36347b3e0266d434e7cbf4a4eeb49a4cb3354
PeterOsoo <posoo617@gmail.com>
2020-11-26 19:58:00 +0300

    regex - use regular expressions  to match emails

--- Python-Regular-Expressions/enails.py
+++ Python-Regular-Expressions/enails.py
@@ -9,6 +9,9 @@ rateng-321-ondiek@my-work.net
 
 pattern = re.compile(r'[a-zA-Z0-9.-]+@[a-zA-Z-]+\.(com|edu|net)')
 
+# email address
+pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+
 matches = pattern.finditer(emails)
 
 for match in matches:
